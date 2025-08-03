ae659d44b6bb8d81abd45e579f99b3f522887d0e
Travis <146099262+Humble689@users.noreply.github.com>
2025-08-03 19:28:03 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 import { ThemeProvider, createTheme, CssBaseline } from '@mui/material'
 import StudyBuddy from './StudyBuddy'
 
-
-
-  
-
-
 const theme = createTheme({
   palette: {
     primary: {
